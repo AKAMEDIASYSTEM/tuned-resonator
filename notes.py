b659e7f5608a014234cffc915c9aeb83a2d796c5
AKAMEDIASYSTEM <nfeehan@gmail.com>
2014-12-28 21:00:03 -0500

    added notes for way in the future nltk integration

---
+++ notes.py
@@ -0,0 +1,15 @@
+# this works for getting the following operations to happen without ascii errors, etc:
+# nltk themselves now recommend you use beautifulSoup's get_text()
+# 
+from bs4 import BeautifulSoup
+
+
+
+soup = BeautifulSoup('your_html_file.html.html')
+text = soup.get_text()
+j = open('output.txt','w')
+text = text.encode('ascii', 'ignore').decode('ascii')
+j.write(text)
+j.close()
+
+# text is now saved like regular text in that file, ready for nltk and nlp stuff
