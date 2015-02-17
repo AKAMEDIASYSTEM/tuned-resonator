8133ec02921957bd951d940901bc2441249c7e1b
akamediasystem <nfeehan@gmail.com>
2015-02-17 13:47:58 -0500

    parsing stuff

--- resonator.py
+++ resonator.py
@@ -3,40 +3,13 @@
 # experiments with google physical-web mdns broadcast
 
 '''
-from google's repo:
-The Physical Web is about getting URLs into the physical world.
-However, this isn't limited to just Bluetooth Low Energy (BLE) beacons.
-mDNS is a service broadcast technique used in Wifi networks.
-It has a two advantages over BLE:
+02/2015 revisit; this will now attempt to parse logfiles from tinyproxy
 
-Only people logged into your wifi can see the mDNS URLs. This means that in an appartment, your neighbors can't see your devices.
-It doesn't have the length restrictions of BLE has so a URL can be along as you'd like (well, at least up to 100 characters).
-Below is an example of how to setup a Raspberry Pi to broadcast a Physical Web URL using mDNS. We hope others are willing to contribute and offer more versions.
-If so, we'll create an mDNS directory for all the alternatives.
-
-You'll first need a mDNS service on your RPi. Avahi is the one we use here: $ sudo apt-get install avahi-daemon
-
-You'll then place a '.service' file into the /etc/avahi/services directory. Our sample file 'physical-web-url.service' looks like this:
-
-<?xml version="1.0" standalone='no'?><!--*-nxml-*-->
-<!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-<service-group>
-  <name replace-wildcards="yes">http://www.mycompany.com/xyz.html</name>
-  <service>
-    <host-name>www.mycompany.com</host-name>
-    <type>_http._tcp</type>
-    <port>80</port>
-    <txt-record>path=/xyz.html</txt-record>
-  </service>
-</service-group>
-Then <name> tag must be unique on your network. 
-We are suggesting that you use the URL as your name to be safe.
-That should be it.
-If you have the latest client on your phone, the web page http://www.mycompany.com/xyz.html will now show up in your list of nearby devices.
-Note: the iOS app supports mDNS, but not the Android app yet.
-It will be updated within a few days for Android 5.0 devices.
-If people are stuck on older versions, please let us know (or feel free add it yourself ;-)
-
+copy logfile into local/temp file
+wipe logfile so it can be refilled
+open tempfile
+extract all urls form tempfile
+throw away all urls that end in .[png, svg, gif, jpeg, js, ipa, css] - make this case-insensitive
 '''
 
 from lxml import etree
@@ -46,6 +19,7 @@ import requests
 # import feedparser
 import json
 from subprocess import call
+import eatiht.v2 as v2
 
 localhost = '192.168.1.1'
 local_url = '192.168.0.113'
@@ -55,29 +29,6 @@ call(["cat", "/var/log/remote_aka.log | grep 'trans Host GET' > tempGrep.txt"])
 # call(["echo", "777 > /var/log/remote_aka.log"]) # deprecate this for now while testing
 
 nixList = ['png','jpeg','jpg','css','js','ipa','ico','gif','mov','mp4','svg','json','woff','woff2']
-
-# example: d = feedparser.parse('http://feedparser.org/docs/examples/atom10.xml')
-# d['feed']['title']
-
-
-## curriculum example - total number of keywords in last 7200 minutes, ie 5 days
-# q = requests.get(keys.url, params=keys.params)
-# q.json()
-# print q.text
-
-## feedparser example - number of items in some feed
-# d = feedparser.parse(keys.feedurl)
-# print d['feed']['summary']
-
-'''
-02/2015 revisit; this will now attempt to parse logfiles from tinyproxy
-
-copy logfile into local/temp file
-wipe logfile so it can be refilled
-open tempfile
-extract all urls form tempfile
-throw away all urls that end in .[png, svg, gif, jpeg, js, ipa, css] - make this case-insensitive
-'''
 
 def isValid(line_in):
     # check for jpeg, jpg, gif, js, etc
@@ -104,9 +55,14 @@ with open('tempGrep.txt') as f:
 
 # k = [i for i in k if i.split('.')[-1]=='jpg'] # all jpgs
 # k = [i for i in k if i.split('.')[-1]!='jpg' and i.split('.')[-1]!='png' and i.split('.')[-1]!='js' and i.split('.')[-1]!='ico' and localhost not in i]
-k = [i for i in k if isValid(i)]
-for l in k:
-    print l
+urls = [i for i in k if isValid(i)]
+
+for url in urls:
+    text = v2.extract(url)
+    print text
+    print '\n\n\n\n'
+
+
 
 # with open('tempGrep.txt') as f:
     # content = f.readlines()
@@ -142,3 +98,40 @@ f.close()
 '''
 # here we could also update the <meta> properties of the local website
 
+
+'''
+from google's repo:
+The Physical Web is about getting URLs into the physical world.
+However, this isn't limited to just Bluetooth Low Energy (BLE) beacons.
+mDNS is a service broadcast technique used in Wifi networks.
+It has a two advantages over BLE:
+
+Only people logged into your wifi can see the mDNS URLs. This means that in an appartment, your neighbors can't see your devices.
+It doesn't have the length restrictions of BLE has so a URL can be along as you'd like (well, at least up to 100 characters).
+Below is an example of how to setup a Raspberry Pi to broadcast a Physical Web URL using mDNS. We hope others are willing to contribute and offer more versions.
+If so, we'll create an mDNS directory for all the alternatives.
+
+You'll first need a mDNS service on your RPi. Avahi is the one we use here: $ sudo apt-get install avahi-daemon
+
+You'll then place a '.service' file into the /etc/avahi/services directory. Our sample file 'physical-web-url.service' looks like this:
+
+<?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+<!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+<service-group>
+  <name replace-wildcards="yes">http://www.mycompany.com/xyz.html</name>
+  <service>
+    <host-name>www.mycompany.com</host-name>
+    <type>_http._tcp</type>
+    <port>80</port>
+    <txt-record>path=/xyz.html</txt-record>
+  </service>
+</service-group>
+Then <name> tag must be unique on your network. 
+We are suggesting that you use the URL as your name to be safe.
+That should be it.
+If you have the latest client on your phone, the web page http://www.mycompany.com/xyz.html will now show up in your list of nearby devices.
+Note: the iOS app supports mDNS, but not the Android app yet.
+It will be updated within a few days for Android 5.0 devices.
+If people are stuck on older versions, please let us know (or feel free add it yourself ;-)
+
+'''
