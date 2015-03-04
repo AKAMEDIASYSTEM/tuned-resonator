45a523444c0d29a97e43d4c3fde8447d0d04c3f3
akamediasystem <nfeehan@gmail.com>
2015-03-04 13:38:54 -0500

    mimetype validation is failing

--- worker.py
+++ worker.py
@@ -15,7 +15,7 @@ while beanstalk.peek_ready():
     url = URL(job.body)
     try:
         # s = url.download(timeout=2500)
-        s = url.download()
+        s = url.download(cache=True,timeout=100)
         the_type = url.mimetype
         print the_type
         if url.mimetype==MIMETYPE_WEBPAGE or url.mimetype==MIMETYPE_PLAINTEXT:
