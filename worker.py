1ee0f05760f80d635d71136b19c36d235b43eb30
AKAMEDIASYSTEM <nfeehan@gmail.com>
2015-03-03 22:42:59 -0500

    adding worker

--- worker.py
+++ worker.py
@@ -7,18 +7,18 @@ from pattern.web import URL, plaintext
 
 beanstalk = beanstalkc.Connection(host='localhost', port=14711)
 job = beanstalk.reserve()
-output = open('test_output_redis.txt','w')
+# output = open('test_output_redis.txt','w')
 url = URL(job.body)
 try:
     s = url.download(timeout=25)
     the_type = url.mimetype
     if 'text/html'==the_type:
         s = plaintext(s)
-        output.write(s.encode('ascii','ignore'))
-        print line
+        # output.write(s.encode('ascii','ignore'))
+        print s
         c = c+1
         print c
 except:
     print 'timeout on ', line
-
+# output.close()
 job.delete()
