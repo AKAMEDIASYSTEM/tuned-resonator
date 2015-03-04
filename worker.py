aed2373c05ab63a2326de48b3b6bb0168b750c34
AKAMEDIASYSTEM <nfeehan@gmail.com>
2015-03-03 22:43:35 -0500

    adding worker

--- worker.py
+++ worker.py
@@ -9,6 +9,7 @@ beanstalk = beanstalkc.Connection(host='localhost', port=14711)
 job = beanstalk.reserve()
 # output = open('test_output_redis.txt','w')
 url = URL(job.body)
+print 'url is ',url
 try:
     s = url.download(timeout=25)
     the_type = url.mimetype
@@ -19,6 +20,6 @@ try:
         c = c+1
         print c
 except:
-    print 'timeout on ', line
+    print 'timeout on ', url
 # output.close()
 job.delete()
