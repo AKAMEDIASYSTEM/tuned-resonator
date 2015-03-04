65827d3e27117ae414762f3cc8a70becac814341
AKAMEDIASYSTEM <nfeehan@gmail.com>
2015-03-03 22:49:21 -0500

    adding worker

--- worker.py
+++ worker.py
@@ -5,22 +5,38 @@
 import beanstalkc
 from pattern.web import URL, plaintext
 
+def isValid(line_in):
+    # check for jpeg, jpg, gif, js, etc
+    # return True if it's a valid url
+    if 'Host'==line_in:
+        return False
+    try:
+        p = parse(line_in)
+        for n in nixList:
+            if p.path.endswith(n):
+                return False
+            if localhost in p.netloc:
+                return False
+        return True
+    except:
+        return False
+
 beanstalk = beanstalkc.Connection(host='localhost', port=14711)
 job = beanstalk.reserve()
-print job
+print job.body
 # output = open('test_output_redis.txt','w')
 url = URL(job.body)
-print 'url is ',url
-try:
-    s = url.download(timeout=2500)
-    the_type = url.mimetype
-    if 'text/html'==the_type:
-        s = plaintext(s)
-        # output.write(s.encode('ascii','ignore'))
-        print s
-        c = c+1
-        print c
-except:
-    print 'timeout on ', url
+if(isValid(job.body)):
+    try:
+        s = url.download(timeout=2500)
+        the_type = url.mimetype
+        if 'text/html'==the_type:
+            s = plaintext(s)
+            # output.write(s.encode('ascii','ignore'))
+            print s
+            c = c+1
+            print c
+    except:
+        print 'timeout on ', url
 # output.close()
 job.delete()
