0d824d1f67617390b031351493d4fe696a0ec6a6
akamediasystem <nfeehan@gmail.com>
2015-03-04 15:25:12 -0500

    still not writing to file right, but seem to be doing better than this morning

--- worker.py
+++ worker.py
@@ -6,16 +6,16 @@ import beanstalkc
 from pattern.web import URL, plaintext, URLError, MIMETYPE_WEBPAGE, MIMETYPE_PLAINTEXT, HTTPError
 from urlparse import urlparse as parse
 import sys
+import redis
+
 
 
 beanstalk = beanstalkc.Connection(host='localhost', port=14711)
 c=0
+output = open('test_output_redis.txt','w')
 while beanstalk.peek_ready():
     job = beanstalk.reserve()
-    output = open('test_output_redis.txt','w')
-    # print url
     url = URL(job.body)
-
     try:
         # s = url.download(timeout=2500)
         # s = url.download(user_agent='Mozilla/5.0')
@@ -28,15 +28,13 @@ while beanstalk.peek_ready():
             # print s.encode('ascii','ignore')
             c = c+1
             print c
-            output.close()
         else:
             'we failed the mimetype test again wtf'
-            output.close()
     # except URLError, e:
     except HTTPError, e:
         # e = sys.exc_info()[0]
         # print 'URLError on ', url
         print e
         output.close()
-    
+    output.close()
     job.delete()
