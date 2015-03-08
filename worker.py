f14e9a7cb7df1b6ad1b9ba7cb12342b2b100e577
AKAMEDIASYSTEM <nfeehan@gmail.com>
2015-03-08 14:48:35 -0400

    changing redis stuff to pipelined

--- worker.py
+++ worker.py
@@ -27,7 +27,7 @@ while True:
     job = beanstalk.reserve() # this is blocking, waits till there's something on the stalk
     url = URL(job.body)
     pipe = r.pipeline(transaction=True)
-    redis_response = pipe.incr(url).expire(url, EXPIRE_IN).execute()
+    redis_response = pipe.incr(url).expire(url, EXPIRE_IN).execute() # should I be updating the TTL? Experience-design question more than anything
     print redis_response
 
     if(redis_response[0] < 2):
@@ -36,7 +36,25 @@ while True:
             s = url.download(cached=True)
             print url.mimetype
             if (url.mimetype in MIMETYPE_WEBPAGE) or (url.mimetype in MIMETYPE_PLAINTEXT):
-                s = plaintext(s)
+                # s = plaintext(s)
+                '''
+                parsetree(string,
+                       tokenize = True,         # Split punctuation marks from words?
+                           tags = True,         # Parse part-of-speech tags? (NN, JJ, ...)
+                         chunks = True,         # Parse chunks? (NP, VP, PNP, ...)
+                      relations = False,        # Parse chunk relations? (-SBJ, -OBJ, ...)
+                        lemmata = False,        # Parse lemmata? (ate => eat)
+                       encoding = 'utf-8'       # Input string encoding.
+                         tagset = None)         # Penn Treebank II (default) or UNIVERSAL.
+'''
+                parsed = text_parse(plaintext(s),
+                    tokenize = False,
+                    tags = False,
+                    chunks = True,
+                    relations = False,
+                    lemmata = False,
+                    tagset = None)
+                print parsed
                 # do noun phrase extraction, add to redis store
                 # output.write(s.encode('ascii','ignore')) # deprecated, was for debug
                 # print s.encode('ascii','ignore')
