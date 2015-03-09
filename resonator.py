d88ecd30aea1bbe210ab80c6743574374496f5b3
AKAMEDIASYSTEM <nfeehan@gmail.com>
2015-03-09 00:28:41 -0400

    systemd cleanup and tuning

--- resonator.py
+++ resonator.py
@@ -20,8 +20,7 @@ zeitgeist = r_text.randomkey()
 root = objectify.Element('service-group')
 t_name = objectify.SubElement(root, 'name')
 t_name.attrib['replace-wildcards'] = 'yes'
-tempText = 'CURRICULUM:'+zeitgeist
-t_name._setText('CURRICULUM-BARNACLE')
+t_name._setText('donald.lan')
 t_service = objectify.SubElement(root, 'service')
 tt_hostname = objectify.SubElement(t_service, 'host-name')
 tt_hostname._setText(hostname)
