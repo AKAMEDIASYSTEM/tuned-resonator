7b0c0375bd6c642e1f2728d41c54ed8569686ee4
AKAMEDIASYSTEM <nfeehan@gmail.com>
2015-03-08 23:36:16 -0400

    systemd stuff again

--- resonator.py
+++ resonator.py
@@ -10,27 +10,17 @@ it queries a random term from recent browsing and updates the .service file that
 
 from lxml import etree
 from lxml import objectify
-import requests
-# import keys
-# import feedparser
-import json
-from subprocess import call
-import eatiht.v2 as v2
-import os
-import time
-from urlparse import urlparse as parse
-import nltk
-# from nltk.corpus import brown
-import pattern.web
+import redis
 
-localhost = '192.168.1.1'
 hostname = 'donald.lan'
+r_text = redis.StrictRedis(host='localhost', port=6379, db=1)
+zeitgeist = r_text.randomkey()
 
 # create XML 
 root = objectify.Element('service-group')
 t_name = objectify.SubElement(root, 'name')
 t_name.attrib['replace-wildcards'] = 'yes'
-t_name._setText(hostname)
+t_name._setText('http://donald.lan/?t=Minutes_into_the_past')
 t_service = objectify.SubElement(root, 'service')
 tt_hostname = objectify.SubElement(t_service, 'host-name')
 tt_hostname._setText(hostname)
@@ -39,7 +29,7 @@ tt_type._setText('_http._tcp')
 tt_port = objectify.SubElement(t_service, 'port')
 tt_port._setText('80')
 tt_txtrecord = objectify.SubElement(t_service, 'txt-record')
-tt_txtrecord._setText('path=/')
+tt_txtrecord._setText('path=/'+zeitgeist)
 objectify.deannotate(root, cleanup_namespaces=True)
 s = '<?xml version="1.0" standalone="no"?><!--*-nxml-*--><!DOCTYPE service-group SYSTEM "avahi-service.dtd">'+etree.tostring(root, pretty_print=True)
 # print s
