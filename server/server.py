79065cfa848ed756b6f5dd7c108025f44cedd350
akamediasystem <nfeehan@gmail.com>
2015-03-27 14:37:25 -0400

    tiny server cleanup

--- server/server.py
+++ server/server.py
@@ -7,24 +7,15 @@ import tornado.web
 from handlers.BrowserHandler import BrowserHandler
 from handlers.ApiHandler import ApiHandler
 import redis
-# from handlers.APIHandler import APIHandler
 
+settings = {'debug':True}
 db = redis.StrictRedis(host='localhost', port=6379, db=1)
-
-settings = dict(
-    #template_path=os.path.join(os.path.pardir(__file__), "templates"),
-    # template_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'../', 'templates')),
-    # static_path=os.path.abspath(os.path.join(os.path.dirname( __file__ ),'../', "static")),
-    debug=True
-)
 
 application = tornado.web.Application([
     (r"/api", ApiHandler),
     (r"/", BrowserHandler),
-    # (r"/api", APIHandler)
 ], db=db, **settings)
 
 if __name__ == "__main__":
-
     application.listen(80)
     tornado.ioloop.IOLoop.instance().start()
