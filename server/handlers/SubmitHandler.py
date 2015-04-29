6a850ec96f444dd2679e410edecd4abf7be8e224
akamediasystem <nfeehan@gmail.com>
2015-04-29 16:12:36 -0400

    updates from more recent curriculum projects (insular, docker et al)

---
+++ server/handlers/SubmitHandler.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# tuned-resonator
+# experiments with google physical-web mdns broadcast
+
+from handlers.BaseHandler import BaseHandler
+from ResponseObject import ResponseObject
+import beanstalkc
+
+
+class SubmitHandler(BaseHandler):
+    """json submission to curriculum-insular store"""
+
+    def post(self):
+        beanstalk = beanstalkc.Connection(host='localhost', port=14711, parse_yaml=False)
+        url = self.get_argument('url', None)
+        print 'inside curriculum-insular SubmitHandler', url
+        if url is not None:
+            beanstalk.put(url)
+
+        self.response = ResponseObject('200', 'Success')
+        self.write_response()
+        self.finish()
